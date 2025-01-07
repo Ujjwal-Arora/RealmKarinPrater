592f9471dbdf3c589cae6354fd64bc1a47696445
Ujjwal Arora <ukarora9@gmail.com>
2025-01-08 00:46:42 +0530

    Initial Commit

---
+++ RealmKarinPrater/RealmKarinPraterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmKarinPraterApp.swift
+//  RealmKarinPrater
+//
+//  Created by Ujjwal Arora on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RealmKarinPraterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
